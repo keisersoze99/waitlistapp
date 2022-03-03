d1279509a71b68ff47e8e53584aae67bda799334
Harish <hkannarg@deloitte.com>
2022-03-03 17:35:28 +0530

    Added redirection logic to redirect user to confirmation page

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,25 @@ Router.get('/contact', (req, res) => {
     res.json({phn: 8675811997});
 });
 
+Router.get('/waitlist/:id', (req, res) => {
+    
+    console.log(`id is ${req.params.id}`);
+    let id = req.params.id;
+    let query = 'SELECT * FROM waitlist WHERE \`sequence\` = \''+id+'\' ;';
+    dbConnection.query(query, (err, result) => {
+        if(err) {
+            console.log('error');
+        } 
+        if(result) {
+            if(result.length > 0) {
+                let user = result[0];
+                res.render('confirmation', {user});
+            } 
+            
+        }
+    })
+})
+
 Router.post('/register', (req, res) => {
     
     let email = req.body.email;
@@ -21,7 +40,7 @@ Router.post('/register', (req, res) => {
             res. send(err);
         } else {
             let position = result.insertId;
-            res.send({position});
+            res.redirect(`/waitlist/${position}`);
         }
         
     })
